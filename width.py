bc835126fa157f480b109a884f49169d4a08238c
Nicole Schneider <nschneid@cs.loyola.edu>
2019-03-01 22:28:22 -0500

    import PIL vs Pillow issue

--- width.py
+++ width.py
@@ -1,1 +1,15 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
+import PIL
+import imageio
+
+
+# process image of necked specimen to determine width
+# Nicole Schneider
+
+## Scaling
+
+scale = matplotlib.pyplot.imread('.01mm.jpg');
+#scale = imread('2018_scale.tif');
